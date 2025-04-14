b6c766c7ec550dbb8bb7f96803bc26881cfd162c
Muhammad Sarwar <sarwarkhalid69@gmail.com>
2025-04-14 23:21:06 +0500

    second commit

--- fylo-landing-page/src/App.jsx
+++ fylo-landing-page/src/App.jsx
@@ -1,9 +1,9 @@
-import Navbar from "./navbar";
-import Header from "./header";
+import Navbar from "./Navbar";
+import Header from "./Header";
 import Features from "./Features";
 import ProductiveSection from "./Productive-section";
 import Testimonials from "./Testimonials";
-import Card from "./card";
+import Card from "./Card";
 import Footer from "./Footer";
 
 function App() {
