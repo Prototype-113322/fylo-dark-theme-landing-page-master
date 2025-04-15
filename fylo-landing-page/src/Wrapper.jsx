c6da86804c446bb0842d6f0c1cb0519783023e40
Muhammad Sarwar <sarwarkhalid69@gmail.com>
2025-04-15 23:35:29 +0500

    Fourth Commit

--- fylo-landing-page/src/Wrapper.jsx
+++ fylo-landing-page/src/Wrapper.jsx
@@ -4,17 +4,24 @@ import Features from "./Features";
 import ProductiveSection from "./Productive-section";
 import Testimonials from "./Testimonials";
 import Card from "./Card";
+import { motion } from "framer-motion";
 
 function Wrapper() {
   return (
     <main className="wrapper-desktop">
       <section className="sub-wrapper">
-        <Navbar></Navbar>
-        <Header></Header>
-        <Features></Features>
-        <ProductiveSection></ProductiveSection>
-        <Testimonials></Testimonials>
-        <Card></Card>
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ delay: 0.5, duration: 1 }}
+        >
+          <Navbar></Navbar>
+          <Header></Header>
+          <Features></Features>
+          <ProductiveSection></ProductiveSection>
+          <Testimonials></Testimonials>
+          <Card></Card>
+        </motion.div>
       </section>
     </main>
   );
